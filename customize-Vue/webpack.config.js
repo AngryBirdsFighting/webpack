fb2a7bf3e6b04a931787f88a402f4e73f128143e
Loki Wang王朝 <15903557434@sina.cn>
2019-02-27 17:22:27 +0800

    修改 配置

--- customize-Vue/webpack.config.js
+++ customize-Vue/webpack.config.js
@@ -2,16 +2,23 @@
 var path = require("path")
 const webpack = require('webpack');//引入webpack
 var htmlWebpackPlugin = require("html-webpack-plugin")
-const ExtractTextPlugin = require("extract-text-webpack-plugin"); // 提取CSS webpack4不建议使用
+//const ExtractTextPlugin = require("extract-text-webpack-plugin"); // 提取CSS webpack4不建议使用
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 提取CSS webpack4使用
 const VueLoaderPlugin = require('vue-loader/lib/plugin')// 处理.vue 文件必须引用这个插件
-const ENV =  process.env.NODE_ENV === 'development';// 判断环境变量
-console.log(ENV)
-let cssLoaders =
+const ENV = process.env.NODE_ENV === 'development';// 判断环境变量
+
+// 生产环境使用 MiniCssExtractPlugin 提取打包css， 开发环境使用 style-loader保证css 热更新
+let cssLoaders = ["css-loader", "postcss-loader"], scssLoaders = ["css-loader", "postcss-loader", "sass-loader"]
+if (ENV) {
+    cssLoaders = ["style-loader", ...cssLoaders], scssLoaders = ["style-loader", ...scssLoaders]
+} else {
+    cssLoaders = [MiniCssExtractPlugin.loader, ...cssLoaders], scssLoaders = [MiniCssExtractPlugin.loader, ...scssLoaders]
+}
 module.exports = {
+    mode: process.env.NODE_ENV, // mode 必须设置，不设置会弹出警告， 或者再 scripts 配置 --mode development
     // 进口文件
     entry: "./src/index.js",
-    devtool: ENV ?'inline-source-map' : "",
+    devtool: ENV ? 'inline-source-map' : false, // 开发环境使用source-map
     // 输出文件
     output: {
         // publicPath:'/dist',//必须加publicPath
@@ -43,21 +50,20 @@ module.exports = {
             // }
         }, {
             test: /\.css$/,
-            use: ["style-loader", "css-loader", "postcss-loader"],
-            // use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+            use: cssLoaders,
         }, {
             test: /\.scss$/,
-            use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
-            // use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+            use: scssLoaders,
         }, {
             test: /\.js$/,
             use: 'babel-loader',
             exclude: path.resolve(__dirname, 'node_modules'), //排除babel解析文件
             include: path.resolve(__dirname, "src") // 指定babel解析文件
         },
-         { 
-             test: /\.(ttf|eot|svg|woff|woff2)$/, 
-             use: 'url-loader' }, // 处理 字体文件的 loader 
+        {
+            test: /\.(ttf|eot|svg|woff|woff2)$/,
+            use: 'url-loader'
+        }, // 处理 字体文件的 loader 
         ]
 
     },
@@ -71,10 +77,13 @@ module.exports = {
         }),
         //热更新插件
         new webpack.HotModuleReplacementPlugin(),
-        // new MiniCssExtractPlugin({
-        //     filename: "css/[name][hash].css"                     // 提取出来的css文件路径以及命名
-        // }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name][hash].css"                     // 提取出来的css文件路径以及命名
+        }),
     ],
+    performance: {
+        hints: false // 性能设置 解决 WARNING in asset size limit: The following asset(s) exceed the recommended size limit
+    },
     //开发服务功能
     devServer: {
         contentBase: path.join(__dirname, "../dist"), //网站的根目录为 根目录/dist，如果配置不对，会报Cannot GET /错误
